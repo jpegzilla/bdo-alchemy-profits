8a594e6f3bdb917990b2ae26e8e29aecd8735282
eris <eris@jpegzilla.com>
2025-04-17 13:54:21 -0400

    remove some unused files

--- webpack.config.mjs
+++
@@ -1,9 +0,0 @@
-export default {
-  mode: 'production',
-  entry: './main.mjs',
-  target: 'node',
-  output: {
-    path: 'D:/documents/development/bdo-alchemy-profits/dist/wp',
-    chunkFormat: 'commonjs',
-  },
-}
